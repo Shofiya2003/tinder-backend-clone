22bc79b0ded12c969fb3afdcc8e1e91001658213
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-24 23:58:49 +0530

    feat:added username to session schema

--- models/session.js
+++ models/session.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const SessionSchema = new mongoose.Schema({
     sessionId: {type:String,required},
-    userId: {type:mongoose.Types.ObjectId},
+    userId: {type:mongoose.Types.ObjectId,required},
+    username: {type:String,required}
 });
 
 const session = mongoose.model('session', SessionSchema);
