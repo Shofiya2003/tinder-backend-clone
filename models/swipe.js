6adb2cfbfdfa78fa9a29263e83c514150130b86c
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-25 18:12:43 +0530

    added timestamps to the schema

--- models/swipe.js
+++ models/swipe.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const SwipeSchema = new mongoose.Schema({
     swipedBy: { type: mongoose.Types.ObjectId, required: true },
     swipedOn: { type: mongoose.Types.ObjectId, required: true },
-    swiped: { type: Boolean, required: true }
+    swiped: { type: Boolean, required: true },
+
+}, {
+    timestamps: true
 });
 
 const swipe = mongoose.model('swipe', SwipeSchema);
