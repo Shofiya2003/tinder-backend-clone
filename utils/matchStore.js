f51b4303af0a7263962622b2a6f57615eba2aed1
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-26 23:26:59 +0530

    feat:added a match store

---
+++ utils/matchStore.js
@@ -0,0 +1,32 @@
+const Swipe = require('../models/swipe')
+const Match = require('../models/match')
+class MatchStore {
+    async checkMatch(swipedByUserId, swipedOnUserId) {
+        try {
+            const swipe = await Swipe.findOne({
+                swipedBy: swipedByUserId,
+                swipedOn: swipedOnUserId,
+                swiped: "right"
+            })
+
+            return swipe;
+        } catch (err) {
+            throw new Error(err.message)
+        }
+
+    }
+
+    async createMatch({ userId1, userId2 }) {
+        try {
+            const match = await Match.create({
+                userId1: userId1,
+                userId2: userId2
+            })
+
+        } catch (err) {
+            throw new Error(err.message)
+        }
+    }
+}
+
+module.exports = new MatchStore();
