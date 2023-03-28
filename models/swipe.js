d261bb8f9da44ecc0f3de2bf266cd53b0012c2e5
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-28 22:51:08 +0530

    made changes to the swipe schema

--- models/swipe.js
+++ models/swipe.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const SwipeSchema = new mongoose.Schema({
     swipedBy: { type: mongoose.Types.ObjectId, required: true },
     swipedOn: { type: mongoose.Types.ObjectId, required: true },
-    swiped: { type: Boolean, required: true },
+    swiped: { type: String, required: true },
 
 }, {
     timestamps: true
