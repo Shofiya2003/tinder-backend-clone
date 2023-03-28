4540335d3936f4d48c2991cc6d0c15d1c76aeee2
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-28 22:50:12 +0530

    feat:added swipe right feature

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const bodyParser = require('body-parser');
 const Developer = require('./models/userInfo');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session')
-const {createServer} = require('http')
+const { createServer } = require('http')
+const { Server } = require("socket.io")
+const generateUniqueId = require('generate-unique-id');
+const jwt = require('jsonwebtoken')
+
 // Importing the api route
 const api = require('./Routes/api');
+
+const sessionStore = require('./utils/sessionStore')
+const matchStore = require('./utils/matchStore')
 
 const cors = require('cors');
 require('dotenv').config()
@@ -28,7 +35,99 @@ app.use(cookieSession({
 app.use('/api', api);
 
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
+io.use(async (socket, next) => {
+    try {
+
+        const sessionId = socket.handshake.auth.sessionId;
+        const token = socket.handshake.auth.token;
+
+        // //verify the jwt token
+        // if (!token || !jwt.verify(token, process.env.JWT_SECRET)) {
+        //     return next(new Error("invalid jwt token"))
+        // }
+
+
+        if (sessionId) {
+            //find the session in the db
+            const session = await sessionStore.findSession(sessionId);
+            if (session) {
+
+                return next();
+            }
+        }
+
+        const userId = socket.handshake.headers.userid;
+        const username = socket.handshake.headers.username;
+        console.log(socket.handshake.headers)
+        console.log(userId);
+
+
+        if (!userId) {
+            return next(new Error("invalid userId"))
+        }
+
+        //create a new session
+        socket.sessionId = generateUniqueId({
+            length: 12,
+            useLetters: true
+        });
+        socket.userId = userId;
+        socket.username = username;
+        await sessionStore.createSession({ sessionId: socket.sessionId, userId: userId, username: username })
+        next();
+    } catch (err) {
+        console.log(err);
+        return next(new Error(err.message))
+    }
+
+
+})
+
+io.on("connection", (socket) => {
+    //send connection details to the client
+    socket.emit("session", {
+        sessionId: socket.sessionId,
+        userId: socket.userId
+    })
+
+    //join room named after the userID
+    socket.join(socket.userId);
+
+    //listen for swipe right
+    socket.on("swiped right", async ({ swipedUserId, swipedUsername }) => {
+
+        const match = await matchStore.checkMatch(swipedUserId, socket.userId)
+        console.log(match);
+        if (match) {
+            await matchStore.createMatch({ userId1: socket.userId, userId2: swipedUserId })
+            socket.emit("match", {
+                username: swipedUsername
+            })
+            socket.to(swipedUserId).emit("match", {
+                username: socket.username
+            })
+        }
+        console.log(socket.userId)
+        console.log(swipedUserId)
+        await matchStore.createRightSwipe(socket.userId, swipedUserId);
+        socket.emit("response", {
+            status: "ok"
+        })
+
+    })
+
+    //listen for swipe left
+    socket.on("swiped left", (swipedUserId, swipedUsername) => {
+        //add left swipe to the db
+
+    })
+
+
+    console.log("connected")
+});
 
 
 mongoose.connect('mongodb://localhost:27017/findyoursimrandatabase', () => {
@@ -36,9 +135,7 @@ mongoose.connect('mongodb://localhost:27017/findyoursimrandatabase', () => {
 });
 
 
-
-
 const port = process.env.port || 5000;
-app.listen(port, () => {
+httpServer.listen(port, () => {
     console.log("Server is running on port 5000");
 })
