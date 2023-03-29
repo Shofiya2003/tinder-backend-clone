4cf4bc98828beb2102ef551844707ae65938bcfe
Shofiya2003 <shofiyabootwala@gmail.com>
2023-03-29 23:26:28 +0530

    added fields of user info in the schema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const UserSchema=new mongoose.Schema({
-    username:{type:String,required:true,unique:true},
-    password:{type:String}
-},{collection:'users'});
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String },
+    name: { type: String },
+    age: { type: Number },
+    gender: { type: String, enum: ["male", "female"] },
+    genderInterestedIn: { type: String, enum: ["male", "female", "both"] },
+    bio: { type: String },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
+}, { collection: 'users' });
 
-const user=mongoose.model('user',UserSchema);
+const user = mongoose.model('user', UserSchema);
 
-module.exports=user;
+module.exports = user;
