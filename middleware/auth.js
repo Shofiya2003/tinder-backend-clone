deaba123ff213064163daa2c06e374878e5acb14
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-02 21:36:18 +0530

    refactor:refactored to rename variables

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,26 @@
 const jwt = require("jsonwebtoken")
 const authMiddleware = async (req,res,next)=>{
     try{
-        //if the session does not have the token or the userid delete it 
-        if(!req.session.isPopulated || !req?.session?.token || !req?.session?.userid ){
+        //if the session does not have the token or the userId delete it 
+        if(!req.session.isPopulated || !req?.session?.token || !req?.session?.userId ){
             req.session = null
             return res.json({status:"error",msg:"user not loggedin"})
         }
         
-        const {token,userid} = req.session
+        const {token,userId} = req.session
         if(!jwt.verify(token,process.env.JWT_SECRET)){
             req.session = null;
             return res.json({status:"error",msg:"invalid token"})
         }
+        
 
-        req.userid = userid
+        req.userId = userId
         next();
         
     }catch(err){
         console.log(err)
         return res.json({status:"error",err:err.message})
-        
+
     }
 }
 
